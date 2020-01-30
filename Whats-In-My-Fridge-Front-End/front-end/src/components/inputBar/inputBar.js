32c7734267088af4eae20a47ab4f980d30ab5c57
Andrew Harris <4ndrew.harri5@gmail.com>
2020-01-30 16:25:48 +0000

    adds tagify to search bar

--- Whats-In-My-Fridge-Front-End/front-end/src/components/inputBar/inputBar.js
+++ Whats-In-My-Fridge-Front-End/front-end/src/components/inputBar/inputBar.js
@@ -1,10 +1,43 @@
 import React, { Component } from 'react';
 import InputStyles from './inputBar.css';
+import Tags from "@yaireo/tagify/dist/react.tagify";
 
-const InputBar = (props) => {
+const baseTagifySettings = {
+    blacklist: [],
+    backspace: "edit",
+    placeholder: "Enter Ingredients",
+    editTags: 1,
+    dropdown: {
+      enabled: 0
+    },
+    callbacks: {}
+};
+
+const InputBar = ({ label, name, initialValue = [], suggestions = [] }) => {
+    const handleChange = e => {
+        console.log(e.type, " ==> ", e.detail.tagify.value.map(item => item.value));
+    };
+
+    const settings = {
+        ...baseTagifySettings,
+        whitelist: suggestions,
+        callbacks: {
+        add: handleChange,
+        remove: handleChange,
+        blur: handleChange,
+        edit: handleChange,
+        invalid: handleChange,
+        click: handleChange,
+        focus: handleChange,
+        "edit:updated": handleChange,
+        "edit:start": handleChange
+        }
+    };
+
     return (
-        <input className="InputBar" placeholder="Enter your ingredients"></input>
-    )
+        <Tags className="InputBar" settings={settings} initialValue={initialValue} />
+    );
+
 }
 
 export default InputBar
